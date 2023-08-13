0f93a6e7a5eb4b7fe0de39f3e8ecc81d6e414a5c
Sergio Lindau <38226617+sergiolindau@users.noreply.github.com>
2023-08-12 23:58:00 -0300

    Initial Commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-export { Evaluator } from './evaluator';
+export { CharString } from './char-string';
+export { Decimal } from 'decimal.js';
+export { ComplexDecimal } from './complex-decimal';
+export { MultiArray } from './multi-array';
+export { Tensor } from './tensor';
+export {
+    TEvaluatorConfig,
+    TBaseFunctionTable,
+    NodeName,
+    NodeExpr,
+    Evaluator,
+} from './evaluator';
